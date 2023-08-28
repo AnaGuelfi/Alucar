42cccca296a5bd93a9307849bdaf7de739e9b510
AnaGuelfi <aguelfie@gmail.com>
2023-08-28 11:13:18 -0300

    Criação do Projeto Spring

---
+++ Alucar/src/main/java/br/ifsp/edu/prss6/alucar/AlucarApplication.java
@@ -0,0 +1,13 @@
+package br.ifsp.edu.prss6.alucar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlucarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlucarApplication.class, args);
+	}
+
+}
---
+++ Alucar/src/test/java/br/ifsp/edu/prss6/alucar/AlucarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.edu.prss6.alucar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlucarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
