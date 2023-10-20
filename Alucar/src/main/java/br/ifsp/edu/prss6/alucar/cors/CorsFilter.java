44e558f985ff55297158af08ccad8c8fe756a84a
Stefano <stefanofog@gmail.com>
2023-10-20 11:16:20 -0300

    Alteração backend para Conexão com Angular

--- Alucar/src/main/java/br/ifsp/edu/prss6/alucar/cors/CorsFilter.java
+++ Alucar/src/main/java/br/ifsp/edu/prss6/alucar/cors/CorsFilter.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-	private String originPermitida = "http://localhost:8082"; 
+	private String originPermitida = "http://localhost:4200";
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
