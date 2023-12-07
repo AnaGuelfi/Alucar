0492f352fd8e9b4a348096fa399aa4113dce4165
Stefano <stefanofog@gmail.com>
2023-12-07 09:51:57 -0300

    Inserção do Campo de Quilometragem

--- Alucar/src/main/java/br/ifsp/edu/prss6/alucar/config/AuthorizationServerConfig.java
+++ Alucar/src/main/java/br/ifsp/edu/prss6/alucar/config/AuthorizationServerConfig.java
@@ -36,8 +36,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.secret("$2a$10$f.yKGdqWDD2pSbVHxzXrh.3umI6pPRQ/D30yIMIrT4aHaZO.xBmTK")
 			.scopes("read", "write")
 			.authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(300)
-			.refreshTokenValiditySeconds(480);
+			.accessTokenValiditySeconds(600)
+			.refreshTokenValiditySeconds(780);
 	}
 	
 	@Override
